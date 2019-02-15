76e7ba1ec5a84e1c5692488831e9615062a696c7
Yoel Monzon <yoelfme@hotmail.com>
2019-02-15 17:13:57 -0600

    [update] add jest configuration and rewrite some tests

---
+++ tests/_stubs/plugins/bad/plugin.js
@@ -0,0 +1,6 @@
+module.exports = {
+  register: () => {
+    throw new Error('bad plugin')
+  },
+  name: 'bad-1-plugin',
+}
--- tests/config.test.js
+++ tests/config.test.js
@@ -1,5 +1,3 @@
-'use strict'
-
 const Config = require('../config')
 
 describe('get data from config', () => {
